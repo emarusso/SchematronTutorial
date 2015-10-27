71418372a4d4e97bb85919ec97c1760cb22af821
Ema <emanuele.russo.benazzi@gmail.com>
2015-10-27 22:10:52 +0100

    removed absolute path to saxon

--- Schematron.bat
+++ Schematron.bat
@@ -1,14 +1,13 @@
 @echo off
 cls
 
-REM Usage: build %%1 = iso schematron file 
-REM  %%2 is the input xml file, with the extension.
-REM E.g. build input input.xml will produce input.report.xml as output
+set XsltProcessor=%SAXON_PATH%
 
-set XsltProcessor=C:\Users\Emanuele\Documents\SchematronIso\SaxonHE9-6-0-7J\saxon9he.jar
+REM use this compiler if you want an output in xml svrl 
 REM set schema_compiler=iso-schematron-xslt2\iso_svrl_for_xslt2.xsl
 set schema_compiler=C:\Users\Emanuele\Documents\SchematronIso\iso-schematron-xslt2\iso_schematron_text.xsl
 
+IF [%SAXON_PATH%]==[] GOTO printrequiresaxon
 IF [%1]==[] GOTO printusage
 
 set schemaFile=%1
@@ -38,6 +37,7 @@ GOTO end
 :printusage
 echo Schematron example requires 2 params: %%1 = iso schematron file, %%2 =  xml to validate.
 echo E.g. %0 input input.xml will produce input.report.xml as output
-
+:printrequiresaxon
+echo SAXON_PATH environment variable requested, found none. Please specify the absolute path of the saxon jar file, e.g. C:\Users\Emanuele\Documents\SaxonHE9-6-0-7J\saxon9he.jar 
 :end
 
